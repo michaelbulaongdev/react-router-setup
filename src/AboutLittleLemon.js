2419c3bf173f732b5312468e1d5129a73da85512
michaelbulaongdev <115378265+michaelbulaongdev@users.noreply.github.com>
2022-11-25 11:04:36 +0800

    Initial commit

---
+++ src/AboutLittleLemon.js
@@ -0,0 +1,5 @@
+function AboutLittleLemon() {
+  return <h1>About Litte Lemon Page</h1>;
+}
+
+export default AboutLittleLemon;
---
+++ src/Contact.js
@@ -0,0 +1,5 @@
+function Contact() {
+  return <h1>Contact us for this page</h1>;
+}
+
+export default Contact;
